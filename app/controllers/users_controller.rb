b2cfe2dfc68ce91fddd1ee46f0286bdeec857fae
Amit Gurav <amitgurav04@gmail.com>
2018-07-15 18:37:02 +0530

    show consents on pdf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
+    @agreed_consents = []
+    consent_categories = @user.consent_categories.map(&:consents)
+    consent_categories.each do |category|
+      @agreed_consents.push(category.first.description) if !category.nil?
+    end
     respond_to do |format|
       format.html
       format.pdf do
